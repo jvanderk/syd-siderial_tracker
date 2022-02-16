aaee0d697f0b74ae1ee0c6ffbb10ae3e3d1431c4
John van der Koijk <33966414+jvanderk@users.noreply.github.com>
2022-02-16 23:57:07 +0100

    Cleanup mostly done, need to modularize.

--- src/bin/syd.rs
+++ src/bin/syd.rs
@@ -10,10 +10,7 @@
 
 // help from  henrik_alser at https://github.com/kalkyl/f103-rtic/blob/main/src/mono.rs
 
-use defmt_rtt as _; // transport layer for defmt logs
-
-use syd as _; // ?
-
+// star tracker servo device app
 #[rtic::app(device =  stm32f1xx_hal::pac, peripherals = true, dispatchers = [USART1])]
 mod app {
 
@@ -39,30 +36,31 @@ mod app {
         timer::{Tim2NoRemap, Tim4NoRemap, Timer},
     };
 
-    //------------
-    // Wrapper around quadrature encoder interface so it becomes i64 in
-    // stead of u32; The qei from the HAL is overloaded by this name
-    // and the QeiManager is added to the arsenal...
-    use qei::QeiManager;
+    // External crate qei contains a wrapper around quadrature encoder
+    // interface module in HAL so it becomes a i64 in stead of u32
+    // device; The Qei from the HAL is an argument for the constructor
+    // of the manager.
+    use qei as qeimanager;
+    use qeimanager::QeiManager;
 
-    // switch debouncer; used fro rate selection
+    // switch debouncer; used for tracking rate selection
     use debouncr::{debounce_2, Debouncer, Edge, Repeat2};
 
     #[monotonic(binds = TIM3, default = true)]
     type MyMono = MonoTimer<pac::TIM3, 10_000>;
 
-    // Shared resources definition
+    // set up quadrature encoder manager
     type SomePin = PB6<Input<Floating>>;
     type OtherPin = PB7<Input<Floating>>;
     type Qei4 = Qei<stm32::TIM4, Tim4NoRemap, (SomePin, OtherPin)>;
-    type QeiManager4 = QeiManager<Qei4>;
+    type QeiManager4 = qeimanager::QeiManager<Qei4>;
 
     #[shared]
     struct Shared {
         tracking: bool,         // system in tracking state?
         rate_index: usize,      // index in the rating speed table
         setpoint: i64,          // target position for digital servo
-        motor_pos: QeiManager4, //
+        motor_pos: QeiManager4, // motor position interface
     }
 
     // Local resources
@@ -81,11 +79,10 @@ mod app {
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-        defmt::println!("init");
+        defmt::println!("init: starting up");
 
         let mut flash = cx.device.FLASH.constrain();
         let rcc = cx.device.RCC.constrain();
-        // Prepare the alternate function I/O registers
         let mut afio = cx.device.AFIO.constrain();
 
         // Acquire the GPIOC peripherals
@@ -100,8 +97,11 @@ mod app {
             .hclk(32.mhz())
             .pclk1(36.mhz())
             .pclk2(36.mhz())
-            .adcclk(4.mhz())
+            .adcclk(16.mhz())
             .freeze(&mut flash.acr);
+
+        // set up system timer
+        let mono = MyMono::new(cx.device.TIM3, &clocks);
 
         // Set up the onboard LED
         let mut onboard_led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
@@ -111,16 +111,11 @@ mod app {
         let mut signal_led = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
         signal_led.set_low();
 
-        // set up system timer
-        let mono = MyMono::new(cx.device.TIM3, &clocks);
-
-        //------------
         // set up PWM
         let pwm_pins = (
             gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl), // motor ch 1
             gpioa.pa1.into_alternate_push_pull(&mut gpioa.crl), // motor ch 2
         );
-
         let pwm = Timer::tim2(cx.device.TIM2, &clocks).pwm::<Tim2NoRemap, _, _, _>(
             pwm_pins,
             &mut afio.mapr,
@@ -130,19 +125,16 @@ mod app {
         let max_duty = pwm.get_max_duty();
 
         let mut pwm_channels = pwm.split();
-
         pwm_channels.0.enable();
         pwm_channels.1.enable();
 
         let pwm_backward = pwm_channels.0;
         let pwm_forward = pwm_channels.1;
 
-        //------------
         // set up quadrature angle counter
         // use TIM4 as the qei counter
         // ! ensure a pull up resistor in hall sensor circuit is physically present
         // ! stm32f1xx_hal::pwm_input::Pins does not allow into_pull_up_input ?!
-
         let qei_pins = (
             gpiob.pb6.into_floating_input(&mut gpiob.crl),
             gpiob.pb7.into_floating_input(&mut gpiob.crl),
@@ -154,12 +146,10 @@ mod app {
         );
         let motor_pos = QeiManager::new(qei);
 
-        //------------
-        // tracking rate selection control switch input Pullup.
+        // tracking rate selection control switch on PA4; input Pullup.
         let rate_switch = gpioa.pa3.into_pull_up_input(&mut gpioa.crl);
 
-        //------------
-        // kick off the threads; they are recalled using sw timer
+        // kick off the threads; they recall themselves using monotonic sw timer
         main::spawn().ok();
         pid_update::spawn().ok();
         rate_update::spawn().ok();
@@ -187,11 +177,9 @@ mod app {
         )
     }
 
-    //------------
-    // main loop, maintaining count setpoint
+    // main loop, maintaining count setpoint and dealing with retraction
     #[task(
-        shared=[
-	    tracking,
+        shared=[tracking,
 	    motor_pos,
 	    setpoint,
 	    rate_index
@@ -218,18 +206,22 @@ mod app {
         //------------
         // set speed table
         // 1.08 deler compenseert voor foute klok?
-        const TRACKING_SPEED_mARCS_PER_S: [f32; 3] = [15000. / 1.08, 14685. / 1.08, 15041. / 1.08];
+        const correction: f32 = 1.08;
+        const TRACKING_SPEED_mARCS_PER_S: [f32; 3] = [
+            15000. / correction, // Solar
+            14685. / correction, // Lunar
+            15041. / correction, // Siderial
+        ];
 
-        //------------
-        // compute setpoint angle (time * tracking speed)
-        // compute angle setpoint in milli arcseconds (tricky, rounding errors galore)
+        // compute setpoint angle (tracking speed * time since start run)
+        // compute angle setpoint in milli arcseconds (approach inherited from integer arithmetic)
         let angle_setpoint: f32 =
             rate_index.lock(|rate_index| TRACKING_SPEED_mARCS_PER_S[*rate_index] * (t - *epoch));
 
-        //------------
         // mount geometry specific coeficients
         // convert angle in  milli-arcseconds to encoder setpoint
-        let target_count = (6.4
+        // compute setpoint indepent of whether or not we are tracking.
+        let mut target_count: i64 = (6.4
             + angle_setpoint
                 * (2.8938e-3
                     + angle_setpoint
@@ -237,56 +229,54 @@ mod app {
                             + angle_setpoint * (-20.517e-21 + angle_setpoint * 268.44e-30))))
             as i64;
 
+        // this lock checks the position and sets the setpoint
+        // it can be made smaller but then the logic becomes unwieldy, and the code is small anyway.
         (tracking, motor_pos, setpoint).lock(|tracking, motor_pos, setpoint| {
             // get position of motor
             motor_pos.sample().unwrap();
             let position = motor_pos.count();
 
-            // defmt::println!("*reported time since epoch {}", t);
-            // defmt::println!("last t_last_position_change {}", *t_last_position_change);
-            // defmt::println!("dt {}", (t-*t_last_position_change));
-            // defmt::println!("setpoint {}", *setpoint);
-            // defmt::println!("position {}", *last_position);
-
-            //------------
-            // check if motor is stalled; assume we are always moving at second scale
-            // if stalled, decide what to do next
-
+            // record last time that position count changed
             if position != *last_position {
                 *t_last_position_change = t;
             }
-
             *last_position = position;
 
+            // if motor is stalled flip tracking state
             if (t - *t_last_position_change) > 3.0 {
-                // more than x seconds passed since last position change:
-                // stalled
                 defmt::println!("stalled");
+
                 if *tracking {
-                    // stalled and tracking: have to retract
-                    *setpoint = -i64::MAX;
+                    // stalled and tracking
                     *tracking = false;
                 } else {
-                    // stalled and not tracking: we just retracted: start tracking
+                    // stalled and not tracking
                     *tracking = true;
-                    *setpoint = 0;
-                    *epoch = t; // start time of this run
-                    motor_pos.reset(); // set count to 0 at starting point
-                    *t_last_position_change = t; // ensure we don't run into this again immediately
+
+                    // record start time of this run and disarm stalled detection
+                    *epoch = t;
+                    *t_last_position_change = t;
+
+                    // set qei wrapper count and target count to 0
+                    motor_pos.reset();
+                    target_count = 0;
                 }
             }
 
-            //------------
-            // set target count if we're tracking
+            // set target count
             if *tracking {
                 *setpoint = target_count;
+            } else {
+                // retract
+                *setpoint = -i64::MAX;
             }
         }); // lock
 
-        main::spawn_after(ExtU32::millis(5u32)).ok();
+        // about 50 ticks per second, so if we have a repeat frequency of 100 Hz we should be tracking tick by tick.
+        main::spawn_after(ExtU32::millis(10u32)).ok();
     }
 
-    // update the PID loop; skeleton
+    // update the PID loop
     // with help from henrik_alser and adamgreig
     #[task(
         shared = [setpoint,  // setpoint position
@@ -300,86 +290,78 @@ mod app {
         ]
     )]
     fn pid_update(cx: pid_update::Context) {
-        //------------
-        // flip the onboard led
-        let onboard_led = cx.local.onboard_led;
+        let pid_update::LocalResources {
+            onboard_led,
+            integral_prior, // PID parameters
+            error_prior,
+            pwm_backward, // pwm API
+            pwm_forward,
+            pwm_max_duty,
+        } = cx.local;
+
+        // switch on the onboard led (inverted logic)
         onboard_led.set_low();
 
-        //------------
-        // this is the setpoint maintained by the main function
+        // this is the setpoint maintained by the main() function
         let setpoint = cx.shared.setpoint;
 
-        //------------
         // qei and motor control
         let motor_pos = cx.shared.motor_pos;
 
-        let pwm_forward = cx.local.pwm_forward;
-        let pwm_backward = cx.local.pwm_backward;
-        let pwm_max_duty = cx.local.pwm_max_duty;
-
-        //------------
-        // PID parameters; experimentally determined, probably highly suboptimal
+        // PID parameters; experimentally determined, probably suboptimal
         let Kp: f32 = 320.0;
         let Ki: f32 = 32.0;
         let Kd: f32 = 0.01;
         let bias: f32 = *pwm_max_duty as f32 / 10.0;
 
-        //------------
-        // remember from previous iteration
-        let integral_prior = cx.local.integral_prior;
-        let error_prior = cx.local.error_prior;
-
-        (setpoint, motor_pos).lock(|setpoint, motor_pos| {
-            // get position of motor
+        // compute position error
+        // lock returns the error
+        let position_error: f32 = (setpoint, motor_pos).lock(|setpoint, motor_pos| {
             motor_pos.sample().unwrap();
-            let position = motor_pos.count();
-            let position_error: f32 = *setpoint as f32 - position as f32;
-
-            // pid control; assuming 1000 Hz update rate
-            let mut integral = *integral_prior + position_error as f32 / 1000.;
-            let derivative = (position_error - *error_prior) * 1000.;
-            let mut pwm_setting = Kp * position_error + Ki * integral + Kd * derivative + bias;
-
-            //------------
-            // this is to limit transition effect after retraction
-            if abs(integral) > 10000.0 {
-                integral = 0.0 * sign(integral)
-            };
-
-            //------------
-            // save the values for next iteration
-            *integral_prior = integral;
-            *error_prior = position_error;
-
-            //------------
-            // limit the pwm setting to max duty cycle
-            if abs(pwm_setting) > (*pwm_max_duty).into() {
-                pwm_setting = *pwm_max_duty as f32 * sign(pwm_setting);
-            };
-
-            //------------
-            // set the pwm
-            if pwm_setting > 0.0 {
-                pwm_forward.set_duty(pwm_setting as u16);
-                pwm_backward.set_duty(0);
-            } else {
-                pwm_forward.set_duty(0);
-                pwm_backward.set_duty(-pwm_setting as u16);
-            };
+            (*setpoint - motor_pos.count()) as f32
         });
 
-        //------------
+        // pid control; assuming 1000 Hz update rate
+        let mut integral = *integral_prior + position_error / 1000.;
+
+        // this is to limit transition effect after retraction
+        if abs(integral) > 100_000.0 {
+            integral = 0.0
+        };
+
+        let derivative = (position_error - *error_prior) * 1000.;
+
+        let mut pwm_setting = Kp * position_error + Ki * integral + Kd * derivative + bias;
+        // limit the pwm setting to max duty cycle
+        if abs(pwm_setting) > *pwm_max_duty as f32 {
+            pwm_setting = *pwm_max_duty as f32 * sign(pwm_setting);
+        };
+
+        // save the values for next iteration
+        *integral_prior = integral;
+        *error_prior = position_error;
+
+        // set the pwm hw
+        if pwm_setting > 0.0 {
+            // forward
+            pwm_forward.set_duty(pwm_setting as u16);
+            pwm_backward.set_duty(0);
+        } else {
+            // retract
+            pwm_forward.set_duty(0);
+            pwm_backward.set_duty(-pwm_setting as u16);
+        };
+
         // switch of on board LED
         onboard_led.set_high();
 
-        //------------
-        // respawn
+        // respawn 1 kHz
         pid_update::spawn_after(1.millis()).ok();
     } // PID update loop
 
-    //------------
-    // update the angular rotation rate and actuate signal
-    // LED; run 10 times/s, fast enough for human switch control
+    // allow user to control the angular rotation rate and control the
+    // rate indication signal LED; run 10 times/s, fast enough for
+    // tracking human switch control
     #[task(
         shared = [
             rate_index,  // index in rate table
@@ -387,7 +369,7 @@ mod app {
         local = [rate_switch,
                  rate_switch_state,
                  signal_led,
-                 d: u32 = 0 // cycle counter for LED signal
+                 decis: u32 = 0 // cycle counter for LED signal
         ]
     )]
     fn rate_update(cx: rate_update::Context) {
@@ -398,12 +380,12 @@ mod app {
             rate_switch,
             rate_switch_state,
             signal_led,
-            d,
+            decis,
         } = cx.local;
 
         // decimal counter for LED pattern
         // assuming 100ms between calls cycle takes 20 * 0.1 = 2.0 s
-        *d = (*d + 1) % 20;
+        *decis = (*decis + 1) % 20;
 
         // signals in tenths of seconds, start times
         const TRACK_PATTERN: [u32; 3] = [0b1, 0b1001, 0b1001001];
@@ -425,7 +407,7 @@ mod app {
 
         // signal led:
         // switch on if bit# d is on in the pattern of the rate_index
-        if TRACK_PATTERN[r] & (0b1 << *d) > 0 {
+        if TRACK_PATTERN[r] & (0b1 << *decis) > 0 {
             signal_led.set_high();
         } else {
             signal_led.set_low();
